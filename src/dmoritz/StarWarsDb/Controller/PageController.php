bc393bed2fa8b963104259c072ceed5e9ff84257
daimeon <dmoritz79@googlemail.com>
2014-08-06 11:45:13 +0200

    symfony

---
+++ src/dmoritz/StarWarsDb/Controller/PageController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmoritz
+ * Date: 06.08.14
+ * Time: 11:35
+ */
+namespace dmoritz\StarWarsDb\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class PageController extends Controller
+{
+    /**
+     * @Route("/impressum", name="page_impressum")
+     * @Template()
+     */
+    public function impressumAction()
+    {
+        return $this->render('dmoritzStarWarsDb:Page\static:impressum.html.twig');
+    }
+}
